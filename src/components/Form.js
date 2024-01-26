a5bec238e0f3ef8f338bd0e7e475ef0106c85841
GR 19 <g.rangarajan19@gmail.com>
2024-01-26 15:40:53 +0530

    first commit

---
+++ src/components/Form.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import bgImg from '../assets/img1.jpg';
+import { useForm } from 'react-hook-form';
+
+export default function Form() {
+
+    const { register, handleSubmit, watch, formState: { errors } } = useForm()
+    const onSubmit = data => console.log(data);
+
+    // console.log(watch('username'));
+    
+  return (
+    <section>
+        <div className="register">
+            <div className="col-1">
+                <h2>Sign In</h2>
+                <span>Created by Rangarajan G</span><br></br>
+                <span> Register below</span>
+
+                <form id='form' className='flex flex-col' onSubmit={handleSubmit(onSubmit)}>
+                    <input type="text" {...register("username")} placeholder='username' />
+                    <input type="text" {...register("password")} placeholder='password' />
+                    <input type="text" {...register("confirmpwd")} placeholder='confirm password' />
+                    <input type="text" {...register("mobile", { required : true, maxLength: 10 })} placeholder='mobile number' />
+                    {errors.mobile?.type === "required" && "Please enter the mobile no"}
+                    {errors.mobile?.type === "maxLength" && "Max Length Exceed"}
+                    <button className='btn'>Sign In</button>
+                </form>
+
+            </div>
+            <div className="col-2">
+                <img src={bgImg} alt="A car" />
+            </div>
+        </div>
+    </section>
+  )
+}
